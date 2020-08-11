e7a5635dcb79666cbc6426e5d68b96695e4a164f
LiawiFelix <55146311+LiawiFelix@users.noreply.github.com>
2020-08-11 21:54:48 +0800

    Accept

--- Day_of_the_Year.cpp
+++ Day_of_the_Year.cpp
@@ -8,11 +8,11 @@ public:
         //format 2019-02-10
 		int ans=0;
 		int y= stoi(date.substr(0,4));
-		int m= stoi(date.substr(5,7));
-		int d= stoi(date.substr(8,10));
+		int m= stoi(date.substr(5,2));
+		int d= stoi(date.substr(8,2));
 		
 		int month[12]={31,28,31,30,31,30,31,31,30,31,30,31};
-		if(leap(y))
+		if(leep(y))
 			month[1]+=1;
 		
 		for(int i=0;i<m-1;i++)
@@ -20,7 +20,7 @@ public:
 		ans=ans+d;
 		return ans;
     }
-	bool leap(int year){
+	bool leep(int year){
 		if(year%400==0)
 			return true;
 		else if(year%100==0)
