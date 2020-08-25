172420785e0be565dfc1bf518f7e52903209f769
LiawiFelix <55146311+LiawiFelix@users.noreply.github.com>
2020-08-25 11:06:36 +0800

    Create Reverse_Integer.cpp

---
+++ Algorithm Problems/Reverse_Integer.cpp
@@ -0,0 +1,24 @@
+//Reverse Integer
+//Date 26 Aug 2020
+//Source 
+
+class Solution {
+public:
+    int reverse(long long int x) {
+        long long int res=0;
+        bool neg= false;
+        if(x<0){
+            x=-x;
+            neg=true;
+        }
+        while(x>0){
+            res= res*10+ x%10;
+            x=x/10;
+        }
+        if(neg)
+            res=-res;
+        if(res<=-2147483648 || res>=2147483648)
+            return 0;
+        return res;
+    }
+};
