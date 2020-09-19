d1ec6d7d700325aed3556a3b6061fc47d21b3bb2
LiawiFelix <55146311+LiawiFelix@users.noreply.github.com>
2020-09-19 11:39:14 +0800

    Create Best_Time_to_Buy_and_Sell_Stock.cpp

---
+++ Algorithm Problems/Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -0,0 +1,20 @@
+//Best Time to Buy and Sell Stock
+//Date 19 Sept 2020
+//Souce 
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int min=9999999;
+        int ans=0;
+        for(int i=0;i<prices.size();i++){
+            if(prices[i]<min){
+                min= prices[i];
+            }
+            else if(prices[i]-min > ans){
+                ans= prices[i]-min;
+            }
+        }
+        return ans;
+    }
+};
