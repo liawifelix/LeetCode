7305743d0c00c1ac60d2ca81b664a072f17a46b4
LiawiFelix <55146311+LiawiFelix@users.noreply.github.com>
2020-10-05 16:57:34 +0800

    Create Complement_of_Base_10_Integer.cpp

---
+++ Algorithm Problems/Complement_of_Base_10_Integer.cpp
@@ -0,0 +1,33 @@
+//Complement of Base 10 Integer
+//Date 5 Oct 2020
+//Source 
+
+class Solution {
+public:
+    string intbit(int n){
+        string res="";
+        while(n>0){
+            res=res+to_string(n%2);
+            n=n/2;
+        }
+        return res;
+    }
+    
+    int comple(string in){
+        int res=0;
+        for(int i=0;i<in.length();i++){
+            if(in[i]=='0'){
+                cout<<i<<endl;
+                res= res+ pow(2,i);
+            }
+                
+        }
+        return res;
+    }
+    
+    int bitwiseComplement(int N) {
+        if(N==0)
+            return 1;
+        return comple(intbit(N));
+    }
+};
