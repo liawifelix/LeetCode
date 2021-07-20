2f27824a160a3711fc74515eb280d882ecd2c01f
Felix Liawi <55146311+liawifelix@users.noreply.github.com>
2021-07-20 08:47:25 +0800

    Create Lowest_Common_Ancestor_of_a_Binary_Search_Tree.py

---
+++ Algorithm Problems/Lowest_Common_Ancestor_of_a_Binary_Search_Tree.py
@@ -0,0 +1,21 @@
+# 20 July 2021
+
+# Time complexity O(n)
+# Space complexity O(1)
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        while root:
+            if root.val<min(p.val, q.val):
+                root= root.right
+            elif root.val>max(p.val, q.val):
+                root= root.left
+            else:
+                return root
